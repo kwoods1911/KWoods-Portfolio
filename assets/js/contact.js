add1a38e34c76957ec2bc1dd2b78f82c02fcb513
Kwoods <kwoods@timelessmedical.com>
2025-01-18 18:13:28 -0400

    commiting contact form

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,9 +1,29 @@
+const emailPublicKey = "AWQmwtMwdZpw11Saa";
+const formID = "contact_service_id";
+const templateID = "template_yawx4mn";
+
+
+let contactForm;
+let successMessage;
+
+
 (function() {
     // https://dashboard.emailjs.com/admin/account
     emailjs.init({
-      publicKey: "GU5QkrwsLx-ZdzUjc",
+      publicKey: emailPublicKey,
     });
 })();
+
+
+
+let displaySuccessMessage = function (){
+    contactForm = document.getElementById('contact-form');
+    successMessage = document.getElementById('success_message');
+    contactForm.style.display = 'none';
+    successMessage.style.display = 'block';
+
+    //display hidden success message
+}
 
 
 
@@ -11,11 +31,16 @@ window.onload = function() {
     document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault();
         // these IDs from the previous steps
-        emailjs.sendForm('contact_service_id', 'template_yawx4mn', this)
+        emailjs.sendForm(formID, templateID, this)
             .then(() => {
                 console.log('SUCCESS!');
+                // display message to user.
+                // show loading message.
+                //display success message to user.
+                displaySuccessMessage();
             }, (error) => {
                 console.log('FAILED...', error);
+                // display error to screen.
             });
     });
 }
